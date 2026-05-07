1dc09e8b6d56c57d6e39867c4c3eaace11482d6f
Ming Tsai <ming.jia1213@gmail.com>
2026-05-06 23:25:53 -0400

    chore(day-8): add while loop

--- Day8/ceasar_cipher.py
+++ Day8/ceasar_cipher.py
@@ -1,21 +1,35 @@
 import string
+import art
+
+print(art.logo)
 
 # Full alphabet (a-z)
 alphabet_list = list(string.ascii_lowercase)
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
 
-def encrypt(original_text, shift_amount):
-    cipher_text = ''
-    for text in original_text:
-        if text in alphabet_list:
-            shifted_position = alphabet_list.index(text) + shift_amount
-            shifted_position %= len(alphabet_list)
-            cipher_text += alphabet_list[shifted_position]
-        else:
-            cipher_text += text
+closed = False
+while not closed:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
 
-    print(f"Here is the encode result: {cipher_text}")
+    def ceasar_cipher(value, shift_amount, encode_or_decode):
+        result = ''
+        if encode_or_decode == "decode":
+            shift_amount *= -1
+            
+        for characters in value:
+            if characters in alphabet_list:
+                shifted_position = alphabet_list.index(characters) + shift_amount
+                shifted_position %= len(alphabet_list)
+                result += alphabet_list[shifted_position]
+            else:
+                result += characters
+        print(f"Here is the {encode_or_decode}d result: {result}")
 
-encrypt(text, shift)
+    ceasar_cipher(text, shift, direction)
+
+    want_to_go = input("Type 'yes' if you want to go again. Otherwise type 'no'")
+    if not want_to_go.lower() == 'yes':
+        closed = True
+
+print("Good Bye!")
