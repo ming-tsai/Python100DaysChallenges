40aa7f855be5c6f080a8659fc06f987c62088e70
Ming Tsai <ming.jia1213@gmail.com>
2026-05-11 17:43:21 -0400

    chore(day9): add angela version

--- Day9/auction.py
+++ Day9/auction.py
@@ -4,23 +4,26 @@ print(art.logo)
 print("Welcome to the secret auction program.")
 
 close_auction = False
-bidded_dictionary = {}
+bider_dictionary = {}
     
+def find_higest_bider(bider_dictionary):
+    maxed = 0
+    maxed_name = ""
+    for bidded_name in bider_dictionary:
+        if bider_dictionary[bidded_name] > maxed:
+            maxed_name = bidded_name
+            maxed = bider_dictionary[bidded_name]
+
+    print(f"This auction winner is {maxed_name}, with amout {maxed}")
+
 while not close_auction:
     name = input("What is your name? ")
-    bidded = float(input("What is your bid? $"))
-    bidded_dictionary[name] = bidded
+    bider = float(input("What is your bid? $"))
+    bider_dictionary[name] = bider
 
-    has_more_bid = input("Are there other users who want to bid? Yes or ").lower()
-    if has_more_bid == "yes":
+    has_more_bider = input("Are there other users who want to bid? Type 'Yes' or ").lower()
+    if has_more_bider == "yes":
         print("\n" * 20)
     else:
-        maxed = 0
-        maxed_name = ""
-        for bidded_name in bidded_dictionary:
-            if bidded_dictionary[bidded_name] > maxed:
-                maxed_name = bidded_name
-                maxed = bidded_dictionary[bidded_name]
-
-        print(f"This auction winner is {maxed_name}, with amout {maxed}")
+        find_higest_bider(bider_dictionary)
         close_auction = True
